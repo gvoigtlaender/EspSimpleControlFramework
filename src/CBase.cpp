fba653d1e0a63cd97e8e912678de0b46366a867e
Georg Voigtländer <gvoigtlaender@googlemail.com>
2021-10-26 22:16:38 +0200

    update: implement to_string for ESP32

--- src/CBase.cpp
+++ src/CBase.cpp
@@ -1,6 +1,17 @@
 #include <CBase.h>
+#include <stdio.h>
 
 double dmap(double x, double in_min, double in_max, double out_min,
             double out_max) {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+std::string to_string(size_t n) {
+#if defined(ESP8266)
+  return std::to_string(n);
+#elif defined(ESP32)
+  char szTmp[32];
+  snprintf(szTmp, 32, "%u", n);
+  return std::string(szTmp);
+#endif
 }
--- src/CBase.h
+++ src/CBase.h
@@ -1,6 +1,8 @@
 #if !defined _SRC__CBASE_H_
 #define _SRC__CBASE_H_
 
+#include <string>
+
 enum _E_STMRESULT {
   STM_BUSY = 0,
   STM_DONE,
@@ -8,5 +10,7 @@ enum _E_STMRESULT {
 
 double dmap(double x, double in_min, double in_max, double out_min,
             double out_max);
+
+std::string to_string(size_t n);
 
 #endif // _SRC__CBASE_H_
