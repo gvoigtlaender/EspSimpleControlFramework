2f573da22c431c27261bb8b1fabab1e2b710d378
Georg Voigtländer <gvoigtlaender@googlemail.com>
2021-11-03 22:35:35 +0100

    add: file upload to LittleFS

--- src/CUpdater.cpp
+++ src/CUpdater.cpp
@@ -1,11 +1,15 @@
 #include "CUpdater.h"
+#include "CControl.h"
 #include <FS.h>
+#include <LittleFS.h>
 #include <WiFiUdp.h>
 #include <flash_hal.h>
 
 static const char successResponse1[] PROGMEM =
     "<META http-equiv=\"refresh\" content=\"15;URL=/\">Update Success! "
     "Rebooting...";
+
+File fsUploadFile;
 
 CUpdater::CUpdater(ESP8266WebServer *pServer, string sPath,
                    string sTitle /* = "" */, string sHtmlHeader /* = "" */)
@@ -35,16 +39,30 @@ CUpdater::CUpdater(ESP8266WebServer *pServer, string sPath,
       "style=\"display: block;\" action='../' "
       "method='get'>\n<button>Main</button>\n</form>\n";
   sIndexPage +=
-      "<form method = "
-      "'POST' action ='' enctype ="
-      "'multipart/form-data'>\n"
       "<fieldset>\n<legend>Firmware:</legend>\n"
-      "<input type = 'file' accept = "
-      "'.bin,.bin.gz' name = 'firmware'>\n<input type = 'submit' value = "
-      "'Update "
-      "Firmware'>\n"
-      "</fieldset>\n"
+      "<form method = 'POST' action ='' enctype ='multipart/form-data'>\n"
+      "<input type = 'file' accept = '.bin,.bin.gz' name = 'firmware'>\n"
+      "<input type = 'submit' value =  'Update Firmware' >\n"
       "</form>\n"
+      "</fieldset>\n"
+      "<fieldset>\n<legend>FS:</legend>\n"
+      "<form method = 'POST' action ='upload' enctype ='multipart/form-data'>\n"
+      "<input type = 'file' accept = '.*' name = 'file'>\n"
+      "<input type = 'submit' value = 'Upload File'>\n"
+      "</form>\n"
+      "<p>";
+
+  File root = LittleFS.open("/", "r");
+  File file = root.openNextFile();
+
+  while (file) {
+    sIndexPage += string(file.name());
+    sIndexPage += "<p>";
+    file = root.openNextFile();
+  }
+
+  sIndexPage +=
+      "</fieldset>\n"
       /*
       "<fieldset><legend>Firmware:</legend>"
       "<form method = 'POST' action ='' enctype = "
@@ -57,7 +75,6 @@ CUpdater::CUpdater(ESP8266WebServer *pServer, string sPath,
 
   // handler for the /update form page
   m_pServer->on(sPath.c_str(), HTTP_GET, [&]() {
-    Serial.printf("CUpdater 1\n");
     m_pServer->send(200, "text/html", sIndexPage.c_str());
   });
 
@@ -65,7 +82,6 @@ CUpdater::CUpdater(ESP8266WebServer *pServer, string sPath,
   m_pServer->on(
       sPath.c_str(), HTTP_POST,
       [&]() {
-        Serial.printf("CUpdater 2\n");
         if (Update.hasError()) {
           m_pServer->send(200, F("text/html"),
                           String(F("Update error: ")) + _updaterError);
@@ -78,7 +94,6 @@ CUpdater::CUpdater(ESP8266WebServer *pServer, string sPath,
         }
       },
       [&]() {
-        Serial.printf("CUpdater 3\n");
         // handler for the file upload, gets the sketch bytes, and writes
         // them through the Update object
         HTTPUpload &upload = m_pServer->upload();
@@ -88,7 +103,7 @@ CUpdater::CUpdater(ESP8266WebServer *pServer, string sPath,
           Serial.setDebugOutput(true);
 
           WiFiUDP::stopAll();
-          Serial.printf("Update: %s\n", upload.filename.c_str());
+          CControl::Log(CControl::I, "Update: %s", upload.filename.c_str());
           if (upload.name == "filesystem") {
             size_t fsSize = ((size_t)&_FS_end - (size_t)&_FS_start);
             close_all_fs();
@@ -114,7 +129,7 @@ CUpdater::CUpdater(ESP8266WebServer *pServer, string sPath,
                    !_updaterError.length()) {
           if (Update.end(true)) { // true to set the size to the current
                                   // progress
-            Serial.printf("Update Success: %u\nRebooting...\n",
+            CControl::Log(CControl::I, "Update Success: %u\nRebooting...\n",
                           upload.totalSize);
           } else {
             _setUpdaterError();
@@ -122,9 +137,46 @@ CUpdater::CUpdater(ESP8266WebServer *pServer, string sPath,
           Serial.setDebugOutput(false);
         } else if (upload.status == UPLOAD_FILE_ABORTED) {
           Update.end();
-          Serial.println("Update was aborted");
+          CControl::Log(CControl::I, "Update was aborted");
         }
         delay(0);
+      });
+
+  m_pServer->on(
+      "/upload", HTTP_POST, // if the client posts to the upload page
+      [&]() {
+        m_pServer->send(200);
+      }, // Send status 200 (OK) to tell the client we are ready to receive
+      [&]() {
+        HTTPUpload &upload = m_pServer->upload();
+        if (upload.status == UPLOAD_FILE_START) {
+          String filename = upload.filename;
+          if (!filename.startsWith("/"))
+            filename = "/" + filename;
+          CControl::Log(CControl::I, "handleFileUpload Name: %s",
+                        filename.c_str());
+          fsUploadFile = LittleFS.open(
+              filename, "w"); // Open the file for writing in SPIFFS
+                              // (create if it doesn't exist)
+          filename = String();
+        } else if (upload.status == UPLOAD_FILE_WRITE) {
+          if (fsUploadFile)
+            fsUploadFile.write(
+                upload.buf,
+                upload.currentSize); // Write the received bytes to the file
+        } else if (upload.status == UPLOAD_FILE_END) {
+          if (fsUploadFile) {     // If the file was successfully created
+            fsUploadFile.close(); // Close the file again
+            CControl::Log(CControl::I, "handleFileUpload Size: %u",
+                          upload.totalSize);
+            m_pServer->send_P(200, PSTR("text/html"), successResponse1);
+            delay(100);
+            m_pServer->client().stop();
+            ESP.restart();
+          } else {
+            m_pServer->send(500, "text/plain", "500: couldn't create file");
+          }
+        }
       });
 }
 
