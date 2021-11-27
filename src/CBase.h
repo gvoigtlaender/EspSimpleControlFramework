2f4f2d6df8a978a2581bff71ca7ac8d6284d0c13
Georg Voigtländer <gvoigtlaender@googlemail.com>
2021-11-27 14:01:59 +0100

    update: doxygen comments

--- src/CBase.h
+++ src/CBase.h
@@ -1,28 +1,51 @@
 #if !defined _SRC__CBASE_H_
 #define _SRC__CBASE_H_
 
+#include <sstream>
 #include <string>
 
+//! defines return codes of state machines.
+/*!
+ */
 enum _E_STMRESULT {
-  STM_BUSY = 0,
-  STM_DONE,
+  STM_BUSY = 0, /*!< statemachine still busy. */
+  STM_DONE,     /*!< statemachine finished. */
+  STM_ERROR,    /*!< statemachine failed. */
 };
 
+//! Re-maps a number from one range (raw) to another (output).
+/*!
+  \param x current raw value
+  \param in_min minimum raw value.
+  \param in_max maximum raw value.
+  \param out_min minimum output value.
+  \param out_max maximum output value.
+  \return mapped value
+*/
 double dmap(double x, double in_min, double in_max, double out_min,
             double out_max);
 
-// std::string to_string(size_t n);
-#include <sstream>
-
 // https://stackoverflow.com/questions/16605967/set-precision-of-stdto-string-when-converting-floating-point-values
+//! converts floating point variables into string.
+/*!
+  \param a_value an floating point value.
+  \param nPrecision the decimal precision.
+  \return string of value
+*/
 template <typename T>
-std::string to_string_with_precision(const T a_value, const int n = 6) {
+std::string to_string_with_precision(const T a_value,
+                                     const int nPrecision = 6) {
   std::ostringstream out;
-  out.precision(n);
+  out.precision(nPrecision);
   out << std::fixed << a_value;
   return out.str();
 }
 
+//! converts time seconds to a string.
+/*!
+  \param nTimeSeconds the seconds.
+  \return string of value
+*/
 std::string TimeToTimeString(int32_t nTimeSeconds);
 
 extern char szInputType_Text[];
@@ -35,10 +58,20 @@ extern char szInputPattern_MMSS[];
 extern uint32_t g_uiHeapMin;
 extern uint32_t g_uiHeap;
 
+//! Checks free heap. Log if new minimum, or each 1s
+/*!
+ */
 void CheckFreeHeap();
 
+//! Checks remaining free space on LittleFS.
+/*!
+  \return free space in bytes
+*/
 size_t LittleFS_GetFreeSpaceKb();
 
+//! prevents copy of derrived class.
+/*!
+ */
 class CNonCopyable {
 public:
   CNonCopyable() {}
--- src/CButton.h
+++ src/CButton.h
@@ -5,18 +5,34 @@
 #include "CControl.h"
 #include <Arduino.h>
 
+//! CButton. Handle digital input as button, check state, calculate
+//! click state
 class CButton : public CControl {
 public:
-  CButton(uint8_t nPin, uint8_t nMode)
+  //! Constructor.
+  /*!
+    \param nPin ESP digital pin.
+    \param nMode pin mode, default: INPUT_PULLUP.
+  */
+  explicit CButton(uint8_t nPin, uint8_t nMode = INPUT_PULLUP)
       : CControl("CButton"), m_nPin(nPin), m_nMode(nMode),
         m_nDigitalReadRef((nMode == INPUT_PULLUP) ? 0 : 1),
         m_eButtonState(eNone), m_eStateButtonControl(eInit),
         m_uiMillisButtonControl(0), m_uiMillisClick(0),
         m_pMqtt_ButtonState(NULL) {}
+  //! Sets pinMode, creates MQTT variable ButtonState.
+  /*!
+    \return bool if passed successfull
+  */
   bool setup() override;
 
+  //! Polls button state, calculates click state.
+  /*!
+    \param bForce not used.
+  */
   void control(bool bForce /*= false*/) override;
 
+  //! _E_BUTTONSTATE. button (click) state
   enum _E_BUTTONSTATE {
     eNone = 0,
     ePressed,
@@ -26,6 +42,7 @@ public:
     eVeryLongClick
   };
 
+  //! _E_STATE. control state machine
   enum _E_STATE {
     eInit = 0,
     eWaitForHigh,
@@ -34,19 +51,34 @@ public:
     eWaitForLow2
   };
 
-  _E_BUTTONSTATE getBttonState() { return m_eButtonState; }
+  //! Returns button (click) state.
+  /*!
+    \return ButtonState
+    \sa _E_BUTTONSTATE
+  */
+  _E_BUTTONSTATE getButtonState() { return m_eButtonState; }
+  //! Sets button (click) state.
+  /*!
+    \param eState current button (click) state.
+    \sa _E_BUTTONSTATE
+  */
   void setButtonState(_E_BUTTONSTATE eState);
+  //! Returns string of button (click) state.
+  /*!
+    \return string
+    \sa _E_BUTTONSTATE
+  */
   const char *getButtonStateString(_E_BUTTONSTATE eState);
 
 protected:
-  uint8_t m_nPin;
-  uint8_t m_nMode;
-  int8_t m_nDigitalReadRef;
-  _E_BUTTONSTATE m_eButtonState;
-  _E_STATE m_eStateButtonControl;
-  uint64_t m_uiMillisButtonControl;
-  uint64_t m_uiMillisClick;
-  CMqttValue *m_pMqtt_ButtonState;
+  uint8_t m_nPin;                   //! ESP digital pin
+  uint8_t m_nMode;                  //! pin mode, default: INPUT_PULLUP
+  int8_t m_nDigitalReadRef;         //! compare reference according to pin mode
+  _E_BUTTONSTATE m_eButtonState;    //! button (click) state
+  _E_STATE m_eStateButtonControl;   //! control state
+  uint64_t m_uiMillisButtonControl; //! control timer
+  uint64_t m_uiMillisClick;         //! control timer
+  CMqttValue *m_pMqtt_ButtonState;  //! MQTT variable ButtonState
 };
 
 #endif // SRC_CCONTROLBUTTON_H_
