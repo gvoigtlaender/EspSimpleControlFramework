5589d8476dea02975b3f51b1a348747094b6cf3f
Georg Voigtländer <gvoigtlaender@googlemail.com>
2022-07-18 14:30:32 +0200

    add: CSensor

---
+++ src/CSensor.cpp
@@ -0,0 +1,76 @@
+/* Copyright 2019 Georg Voigtlaender gvoigtlaender@googlemail.com */
+#include <CMqtt.h>
+#include <CSensor.h>
+
+bool CSensorBase::setup() {
+  // ValuePending();
+  _log(I, "setup");
+  CreateMqttValues();
+  return true;
+}
+
+void CSensorBase::control(bool bForce) {
+  enum { eStart = 0, eWaitForDelay, eUpdate, eEnd };
+
+  switch (m_nState) {
+  case eStart:
+    firstControlStateAction();
+    m_nState = eWaitForDelay;
+
+  case eWaitForDelay:
+    if (millis() < this->CControl::m_uiTime)
+      break;
+    m_nState = eUpdate;
+
+  case eUpdate:
+    this->CControl::m_uiTime += 2000;
+    if (ReadValues()) {
+      publish();
+      // m_nState = eEnd;
+    }
+    display();
+    m_nState = eWaitForDelay;
+    break;
+
+  case eEnd:
+    break;
+  }
+}
+
+void CSensorSingle::display() {
+  char szTmp[64];
+  snprintf(szTmp, sizeof(szTmp), "T: %02.1f°C", m_Temperature.m_OutputValue);
+  this->_log(I, szTmp);
+  if (m_pDisplayLine != NULL) {
+    string s = string(szTmp) + string("%");
+    m_pDisplayLine->Line(s.c_str());
+  }
+}
+
+void CSensorSingle::publish() {
+
+  m_pMqttTemp->setValue(std::to_string(m_Temperature.m_OutputValue));
+  m_pMqttHum->setValue(std::to_string(m_Humidity.m_OutputValue));
+}
+
+void CSensorMulti::display() {
+  for (uint8_t i = 0; i < m_Sensors.size(); i++) {
+    CSensorChannel *pChannel = m_Sensors[i];
+    if (pChannel->m_pDisplayLine != NULL) {
+      char szTmp[64];
+      snprintf(szTmp, sizeof(szTmp), "T: %02.1f°C",
+               pChannel->m_Temperature.m_OutputValue);
+      string s = string(szTmp) + string("%");
+      pChannel->m_pDisplayLine->Line(s.c_str());
+      this->_log(I, szTmp);
+    }
+  }
+}
+
+void CSensorMulti::publish() {
+  for (uint8_t i = 0; i < m_Sensors.size(); i++) {
+    CSensorChannel *pChannel = m_Sensors[i];
+    pChannel->m_pMqttTemp->setValue(
+        std::to_string(pChannel->m_Temperature.m_OutputValue));
+  }
+}
---
+++ src/CSensor.h
@@ -0,0 +1,237 @@
+/* Copyright 2019 Georg Voigtlaender gvoigtlaender@googlemail.com */
+#ifndef SRC_CSENSOR_H_
+#define SRC_CSENSOR_H_
+
+#include <Arduino.h>
+#include <string>
+using std::string;
+
+#include <Adafruit_BME280.h>
+#include <Adafruit_BMP280.h>
+#include <Adafruit_Sensor.h>
+#include <CControl.h>
+#include <CDisplay.h>
+#include <CFilter.hpp>
+#include <DHT.h>
+#include <OneWire.h>
+#include <SPI.h>
+#include <Wire.h>
+
+#include <DallasTemperature.h>
+#include <vector>
+using std::vector;
+
+class CSensorBase : public CControl {
+public:
+  explicit CSensorBase(const char *szName) : CControl(szName) {}
+
+  bool setup() override;
+  void control(bool bForce) override;
+  virtual void CreateMqttValues() = 0;
+  virtual void firstControlStateAction() {}
+
+  virtual bool ReadValues() = 0;
+
+  virtual void display() = 0;
+  virtual void publish() = 0;
+};
+
+class CSensorSingle : public CSensorBase {
+public:
+  explicit CSensorSingle(const char *szType)
+      : CSensorBase(szType), m_Temperature(10), m_Humidity(10) {}
+  CFilter<float> m_Temperature;
+  CFilter<float> m_Humidity;
+  CMqttValue *m_pMqttTemp = NULL;
+  CMqttValue *m_pMqttHum = NULL;
+
+  void CreateMqttValues() override {
+    m_pMqttTemp = CreateMqttValue("Temperature");
+    m_pMqttHum = CreateMqttValue("Humidity");
+  }
+
+  bool ReadValues() override {
+    float temp = readTemperature();
+    float hum = readHumidity();
+    if (!isnan(temp) && !isnan(hum)) {
+      m_Temperature.Filter(temp);
+      m_Humidity.Filter(hum);
+    }
+    if (m_Temperature.m_nSize > 2)
+      return true;
+    return false;
+  }
+
+  virtual float readTemperature() = 0;
+  virtual float readHumidity() = 0;
+
+  virtual void display() override;
+  virtual void publish() override;
+};
+
+class CSensorDHT : public CSensorSingle {
+private:
+  explicit CSensorDHT(const char *szType)
+      : CSensorSingle(szType), m_pDht(NULL) {}
+
+public:
+  CSensorDHT(const char *szType, uint8_t nPin, uint8_t nType)
+      : CSensorSingle(szType) {
+    m_pDht = new DHT(nPin, nType);
+  }
+  bool setup() override {
+    // m_pDht->begin();
+    return CSensorSingle::setup();
+  }
+  float readTemperature() override { return m_pDht->readTemperature(); }
+  float readHumidity() override { return m_pDht->readHumidity(); }
+  void firstControlStateAction() override { m_pDht->begin(); }
+  DHT *m_pDht;
+};
+
+class CSensorBME280 : public CSensorSingle {
+public:
+  CSensorBME280() : CSensorSingle("BME280") { m_pBME = new Adafruit_BME280(); }
+
+  bool setup() override {
+    // if ( !m_pBME->begin(&Wire) ) {
+    if (!m_pBME->begin(0x76)) {
+      _log(E, "Could not find a valid BME280 sensor, check wiring!");
+      // while (1) { }
+    }
+    return CSensorSingle::setup();
+  }
+  float readTemperature() override { return m_pBME->readTemperature(); }
+  float readHumidity() override { return m_pBME->readHumidity(); }
+
+  Adafruit_BME280 *m_pBME;
+};
+
+class CSensorBMP280 : public CSensorSingle {
+public:
+  CSensorBMP280() : CSensorSingle("BMP280") { m_pBMP = new Adafruit_BMP280(); }
+
+  bool setup() override {
+    if (!m_pBMP->begin()) {
+      _log(E, "Could not find a valid BMP280 sensor, check wiring!");
+      // while (1) { }
+    }
+    return CSensorSingle::setup();
+  }
+  float readTemperature() override { return m_pBMP->readTemperature(); }
+  float readHumidity() override { return 0.0; }
+
+  Adafruit_BMP280 *m_pBMP;
+};
+
+class CSensorMulti : public CSensorBase {
+public:
+  explicit CSensorMulti(const char *szType) : CSensorBase(szType) {}
+  class CSensorChannel {
+  public:
+    CSensorChannel() : m_Temperature(10) /*, m_Humidity(10)*/ {}
+    CMqttValue *m_pMqttTemp = NULL;
+    CDisplayLine *m_pDisplayLine = NULL;
+    // CMqttValue *m_pMqttHum = NULL;
+    CFilter<float> m_Temperature;
+    // CFilter<double> m_Humidity;
+  };
+
+  void CreateMqttValues() override {
+    for (uint8_t i = 0; i < m_Sensors.size(); i++) {
+      m_Sensors[i]->m_pMqttTemp =
+          CreateMqttValue("Temperature\\" + std::to_string(i));
+      // m_Sensors[i]->m_pMqttHum =
+      //    CreateMqttValue("Humidity\\" + std::to_string(i));
+    }
+  }
+
+  virtual void display() override;
+  virtual void publish() override;
+
+  void SetDisplayLine(uint8_t n, CDisplayLine *pDL) {
+    _log(I, "SetDisplayLine(%u, %x)", n, pDL);
+    if (n < m_Sensors.size() && pDL != NULL) {
+      m_Sensors[n]->m_pDisplayLine = pDL;
+    }
+  }
+
+  vector<CSensorChannel *> m_Sensors;
+};
+class CSensorDS18B20 : public CSensorMulti {
+public:
+  CSensorDS18B20(int nPin, OneWire *pOneWire = NULL) : CSensorMulti("DS18B20") {
+    if (pOneWire) {
+      m_pOneWire = pOneWire;
+    } else {
+      m_pOneWire = new OneWire(nPin);
+    }
+    m_pDallas = new DallasTemperature(m_pOneWire);
+
+    m_pDallas->begin();
+    m_pDallas->setWaitForConversion(false);
+    uint8_t uiCnt = m_pDallas->getDeviceCount();
+    _log(I, "Dallas Temperature: %u devices found, %p", uiCnt, m_pDallas);
+    for (uint8_t nCnt = 0; nCnt < uiCnt; nCnt++) {
+      DeviceAddress addr;
+      m_pDallas->getAddress(addr, nCnt);
+      string sAddr = Addr2Str(addr);
+      m_Sensors.push_back(new CSensorChannelDS18B20(addr, sAddr));
+      _log(I, "Addr %u: %s", nCnt, sAddr.c_str());
+    }
+  }
+
+  bool setup() override {
+    m_pDallas->requestTemperatures();
+    this->m_uiTime = millis();
+    return CSensorBase::setup();
+  }
+  bool ReadValues() override {
+    start = millis();
+
+    for (uint8_t nCnt = 0; nCnt < m_Sensors.size(); nCnt++) {
+      CSensorChannelDS18B20 *pSensor =
+          static_cast<CSensorChannelDS18B20 *>(m_Sensors[nCnt]);
+      float t = m_pDallas->getTempC(pSensor->m_Addr);
+      pSensor->m_Temperature.Filter(t);
+      _log(I, "%u Temp: %.1f %s", nCnt, t, pSensor->m_szAddr);
+    }
+
+    m_pDallas->requestTemperatures();
+    stop = millis();
+
+    if ((stop - start) > 100)
+      _log(W, "took %lums", stop - start);
+
+    return (m_Sensors[0]->m_Temperature.m_nSize > 2);
+  }
+
+protected:
+  OneWire *m_pOneWire;
+  DallasTemperature *m_pDallas = NULL;
+  class CSensorChannelDS18B20 : public CSensorChannel {
+  public:
+    CSensorChannelDS18B20(DeviceAddress addr, const string &sAddr)
+        : CSensorChannel() {
+      memcpy(m_Addr, addr, sizeof(DeviceAddress));
+      memcpy(m_szAddr, sAddr.c_str(), 17);
+    }
+    DeviceAddress m_Addr;
+    char m_szAddr[17];
+  };
+
+  string Addr2Str(DeviceAddress deviceAddress) {
+    string s = "";
+    for (uint8_t i = 0; i < 8; i++) {
+      char szTmp[3];
+      sprintf(szTmp, "%02X", deviceAddress[i]);
+      s += szTmp;
+    }
+    return s;
+  }
+
+  uint32_t start, stop;
+  uint8_t res = 12;
+};
+
+#endif // SRC_CSENSOR_H_
