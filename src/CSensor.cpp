90c8a6c9dd99bd857222df9b9398152a08d06fe5
Georg Voigtländer <gvoigtlaender@googlemail.com>
2023-10-14 10:53:49 +0200

    fix: getOutputValue

--- src/CSensor.cpp
+++ src/CSensor.cpp
@@ -50,15 +50,11 @@ void CSensorBase::control(bool bForce) {
 void CSensorSingle::display() {
   /*
   char szTmp[64];
-<<<<<<< Updated upstream
-  snprintf(szTmp, sizeof(szTmp), );
-=======
   snprintf(szTmp, sizeof(szTmp), "T: %02.1f°C", m_Temperature.getOutputValue());
->>>>>>> Stashed changes
   this->_log(I, szTmp);
   */
   std::string sTmp =
-      FormatString<64>("T: %02.1f°C", m_Temperature.getOuptuValue());
+      FormatString<64>("T: %02.1f°C", m_Temperature.getOutputValue());
   this->_log(I, sTmp.c_str());
 #if defined(USE_DISPLAY)
   if (m_pDisplayLine != nullptr) {
@@ -70,13 +66,8 @@ void CSensorSingle::display() {
 void CSensorSingle::publish() {
 
   // _log(I, "publish()");
-<<<<<<< Updated upstream
-  m_pMqttTemp->setValue(std::to_string(m_Temperature.getOuptuValue()));
-  m_pMqttHum->setValue(std::to_string(m_Humidity.getOuptuValue()));
-=======
   m_pMqttTemp->setValue(std::to_string(m_Temperature.getOutputValue()));
   m_pMqttHum->setValue(std::to_string(m_Humidity.getOutputValue()));
->>>>>>> Stashed changes
 }
 
 void CSensorMulti::display() {
@@ -84,11 +75,7 @@ void CSensorMulti::display() {
     if (pChannel->m_pDisplayLine != NULL) {
       char szTmp[32];
       snprintf(szTmp, sizeof(szTmp), "%02.1fC",
-<<<<<<< Updated upstream
-               pChannel->m_Temperature.getOuptuValue());
-=======
                pChannel->m_Temperature.getOutputValue());
->>>>>>> Stashed changes
       pChannel->m_pDisplayLine->Line(szTmp);
       // this->_log(I, szTmp);
     }
@@ -98,11 +85,7 @@ void CSensorMulti::display() {
 void CSensorMulti::publish() {
   for (auto &&pChannel : m_Sensors) {
     pChannel->m_pMqttTemp->setValue(
-<<<<<<< Updated upstream
-        std::to_string(pChannel->m_Temperature.getOuptuValue()));
-=======
         std::to_string(pChannel->m_Temperature.getOutputValue()));
->>>>>>> Stashed changes
   }
 }
 
